1760a71e0a2f3729da3ed42d25a0178eb23b4ba3
Abhishek Kumar Sharma <abhi243461@hotmail.com>
2020-05-23 20:34:24 +0530

    feat: Add method to change state of AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,46 @@ import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 
 export default class AddEntry extends Component {
+
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0
+    }
+
+    increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric)
+
+        this.setState((preState) => {
+            const count = state[metric] + step
+
+            return {
+                ...preState,
+                [metric]: count > max ? max : count
+            }
+        })
+    }
+
+    decrement = (metric) => {
+
+        this.setState((preState) => {
+            const count = state[metric] - getMetricMetaInfo(metric).step
+
+            return {
+                ...preState,
+                [metric]: count < 0 ? 0 : count
+            }
+        })
+    }
+
+    slider = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value,
+        }))
+    }
+
     render() {
         return (
             <View>
