b8636cf80ac553542470cd58e50c291b397e61e2
Abhishek Kumar Sharma <abhi243461@hotmail.com>
2020-05-24 21:14:04 +0530

    feat: Add calendar

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import AddEntry from './components/AddEntry'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)} >
         <View style={{ flex: 1 }}>
-          <AddEntry />
+          <History />
         </View>
       </Provider >
     )
